d86a033ca4d1f809d1dd79c749031d1db14dcd10
ptaina <tainapereirae27@gmail.com>
2024-09-23 11:55:47 -0300

    correção cena

--- IdeaProjects/Gamejava/src/model/Prova.java
+++ IdeaProjects/Gamejava/src/model/Prova.java
@@ -3,7 +3,7 @@ package model;
 public class Prova {
     private Integer provaId;
     private String descricao;
-    private Integer cenaEncontrada;
+    private Cena cenaEncontrada;
     private Integer inventarioId;
 
     public Integer getProvaId() {
@@ -14,27 +14,27 @@ public class Prova {
         this.provaId = provaId;
     }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Integer getCenaEncontrada() {
-        return cenaEncontrada;
-    }
-
-    public void setCenaEncontrada(Integer cenaEncontrada) {
-        this.cenaEncontrada = cenaEncontrada;
-    }
-
     public Integer getInventarioId() {
         return inventarioId;
     }
 
     public void setInventarioId(Integer inventarioId) {
         this.inventarioId = inventarioId;
+    }
+
+    public Cena getCenaEncontrada() {
+        return cenaEncontrada;
+    }
+
+    public void setCenaEncontrada(Cena cenaEncontrada) {
+        this.cenaEncontrada = cenaEncontrada;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 }
